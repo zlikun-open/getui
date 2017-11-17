11cdb230fa67ffeefe34dc4748eb2efb83bbfe37
zlikun <zlikun-dev@hotmail.com>
2017-11-17 19:07:20 +0800

    测试鉴权API

---
+++ src/main/java/com/zlikun/open/AppConstant.java
@@ -0,0 +1,15 @@
+package com.zlikun.open;
+
+/**
+ * @author zlikun <zlikun-dev@hotmail.com>
+ * @date 2017-11-17 18:28
+ */
+public interface AppConstant {
+
+    String APP_ID = "zS8CchJL2nAPGBR4Yw7HP1" ;
+    String APP_SECRET = "fi4zmXd1Ke7esopV2CZPCA" ;
+    String APP_KEY = "vkn7H6wogxAX0XNpdA7hW5" ;
+    String MASTER_SECRET = "xZFlgaxqfh6Tp8hBJxdSN5" ;
+    String CLIENT_ID = "cb9573d8251a8acc984626c19c8dbe5a" ;
+
+}
---
+++ src/test/java/com/zlikun/open/api/AuthTokenTest.java
@@ -0,0 +1,89 @@
+package com.zlikun.open.api;
+
+import com.zlikun.open.AppConstant;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 鉴权API，http://docs.getui.com/server/rest/other_if/
+ * @author zlikun <zlikun-dev@hotmail.com>
+ * @date 2017-11-17 18:22
+ */
+@Slf4j
+public class AuthTokenTest {
+
+    final OkHttpClient client = new OkHttpClient.Builder().build() ;
+
+    /**
+     * 请求鉴权
+     * @throws IOException
+     */
+    @Test
+    public void auth() throws IOException {
+
+        long timestamp = System.currentTimeMillis() ;
+
+        // 生成签名
+        String sign = DigestUtils.sha256Hex(AppConstant.APP_KEY + timestamp + AppConstant.MASTER_SECRET) ;
+        log.info("timestamp = {} ,sign = {}" ,timestamp ,sign);
+
+        // 请求AUTH_TOKEN
+        Request request = new Request.Builder()
+                .url("https://restapi.getui.com/v1/" + AppConstant.APP_ID + "/auth_sign")
+                .addHeader("Content-Type", "application/json")
+                .post(RequestBody.create(MediaType.parse("application/json"),
+                        String.format("{\"sign\":\"%s\",\"timestamp\":\"%s\",\"appkey\":\"%s\"}",
+                                sign, AppConstant.APP_KEY, String.valueOf(timestamp))))
+                .build();
+
+        // 执行请求，并输出响应
+        Response response = client.newCall(request).execute() ;
+
+        log.info("code = {} ,message = {}" ,response.code() ,response.message());
+        assertTrue(response.isSuccessful()) ;
+
+        log.info("/------------\n{}\n------------------------------------------------------------/" ,
+                response.body().string());
+
+    }
+
+    /**
+     * 关闭鉴权
+     * @throws IOException
+     */
+    @Test
+    public void closeAuth() throws IOException {
+
+        long timestamp = System.currentTimeMillis() ;
+
+        // 生成签名
+        String sign = DigestUtils.sha256Hex(AppConstant.APP_KEY + timestamp + AppConstant.MASTER_SECRET) ;
+        log.info("timestamp = {} ,sign = {}" ,timestamp ,sign);
+
+        // 请求AUTH_TOKEN
+        Request request = new Request.Builder()
+                .url("https://restapi.getui.com/v1/" + AppConstant.APP_ID + "/auth_close")
+                .addHeader("Content-Type", "application/json")
+                // POST请求不传参数还不行，R !!!
+                .post(RequestBody.create(MediaType.parse("application/json"),
+                        String.format("timestamp=%s", String.valueOf(timestamp))))
+                .build();
+
+        // 执行请求，并输出响应
+        Response response = client.newCall(request).execute() ;
+
+        log.info("code = {} ,message = {}" ,response.code() ,response.message());
+        assertTrue(response.isSuccessful()) ;
+
+        log.info("/------------\n{}\n------------------------------------------------------------/" ,
+                response.body().string());
+
+    }
+
+}
